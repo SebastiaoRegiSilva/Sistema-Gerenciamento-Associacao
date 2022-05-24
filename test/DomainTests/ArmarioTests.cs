6aa1f197290fe5b8bbdb1d34e544ea846315195d
Sebastião <swillian2009@gmail.com>
2022-05-23 22:06:02 -0300

    Iniciada a implementação do teste automatizado do domínio de armários.

---
+++ test/DomainTests/ArmarioTests.cs
@@ -0,0 +1,38 @@
+using Disparo.Plataforma.Domain.Armarios;
+using Disparo.Plataforma.Infrastructure.Repositories.MongoDb.Armarios;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+
+namespace Disparo.Plataforma.DomainTests
+{
+    /// <summary>Classe de testes para o domínio de alunos.</summary>
+    [TestClass]
+    public class ArmarioTests
+    {
+        /// <summary>Serviço que provê acesso aos dados e operações relacionadas aos armários na plataforma.</summary>
+        private static ArmarioService _armarioService = null;
+
+        /// <summary>Método de preparação para os testes.</summary>
+        [ClassInitialize]
+        public static void PrepareTestsData(TestContext context)
+        {
+            var ptBR = new CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentCulture = ptBR;
+            Thread.CurrentThread.CurrentUICulture = ptBR;
+            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            string conString = config.GetValue<string>("DB:MongoDB:ConString");
+            string database = config.GetValue<string>("DB:MongoDB:Database");
+
+            var armarioRepository = new ArmarioRepository(conString, database);
+            _armarioService = new ArmarioService(armarioRepository);
+        }
+    }
+}
