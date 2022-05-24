250478e7c2a218297e7c252272b40025e444b91f
Sebastião <swillian2009@gmail.com>
2022-05-23 22:22:51 -0300

    Iniciada a implementação do teste automatizado do domínio de prédios.

---
+++ test/DomainTests/PredioTests.cs
@@ -0,0 +1,38 @@
+using Disparo.Plataforma.Infrastructure.Repositories.MongoDb.Predios;
+using Domain.Plataforma.Domain.Predios;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+
+namespace Disparo.Plataforma.DomainTests
+{
+    /// <summary>Classe de testes para o domínio de prédios.</summary>
+    [TestClass]
+    public class PredioTests
+    {
+        /// <summary>Serviço que provê acesso aos dados e operações relacionadas aos prédios na plataforma.</summary>
+        private static PredioService _prediooService = null;
+
+        /// <summary>Método de preparação para os testes.</summary>
+        [ClassInitialize]
+        public static void PrepareTestsData(TestContext context)
+        {
+            var ptBR = new CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentCulture = ptBR;
+            Thread.CurrentThread.CurrentUICulture = ptBR;
+            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            string conString = config.GetValue<string>("DB:MongoDB:ConString");
+            string database = config.GetValue<string>("DB:MongoDB:Database");
+
+            var prediooRepository = new PredioRepository(conString, database);
+            _prediooService = new PredioService(prediooRepository);
+        }
+    }
+}
