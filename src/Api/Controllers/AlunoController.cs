41997d9c9de690894773b3f210791f1439c60029
Sebastião Willian Pereira Regi <swillian2009@gmail.com>
2022-06-17 22:34:20 -0300

    Alterações no controller de Aluno.

--- src/Api/Controllers/AlunoController.cs
+++ src/Api/Controllers/AlunoController.cs
@@ -1,14 +1,17 @@
 using Disparo.Plataforma.Domain.Alunos;
-using Disparo.Plataforma.Domain.Classes;
 using Disparo.Plataforma.Domain.Emails.Exceptions;
+using Disparo.Plataforma.Infrastructure.Repositories.MongoDb.Alunos.Models;
+using Disparo.Plataforma.Infrastructure.Repositories.MongoDb.Classes.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace Disparo.Plataforma.Api.Controllers
 {
     /// <summary>Controller que provê endpoints relacionados a entidade aluno.</summary>
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class AlunoController : Controller
@@ -27,30 +30,35 @@ namespace Disparo.Plataforma.Api.Controllers
         /// <param name="matricula">Matrícula do aluno.</param>
         /// <returns>Um aluno cadastrado no repositório.</returns>
         [HttpGet("{matricula}")]
-        public async Task<ActionResult<Aluno>> BuscarPorMatricula(int matricula)
+        public async Task<ActionResult<AlunoModel>> BuscarPorMatricula(int matricula)
         {
             var aluno = await _alunoService.RecuperarAlunoMatriculaAsync(ConverterIntString(matricula));
             
             return aluno == null? Json($"O aluno com a matrícula {matricula} não existe na base de dados."): Json(aluno); 
         }
 
-        /// <summary> Cadastrar no repositório um aluno.</summary>
+        /// <summary> 
+        /// Cadastrar no repositório um aluno.
+        /// </summary>
         /// <param name="matricula">Matrícula do aluno.</param>
-        /// <param name="dataNascimento">Data de nascimento do aluno.</param>
+        /// <param name="nome">Nome do aluno.</param>
+        /// <param name="dataNascimento">Data de nascimento do aluno.(Formato yyyy-mm-dd)</param>
         /// <param name="enderecoEmail">Endereço institucional do aluno.</param>
         /// <param name="numerosTelefones">Formas de contato telefônico direto com o aluno.</param>
-        /// <param name="classe">Classe que o aluno será cadastradado.</param>
+        /// <param name="classe">Classe que o aluno será cadastrada.</param>
         [HttpPost]
-        public async Task<IActionResult> CadastrarAluno(int matricula, string nome,  DateTime dataNascimento, 
-        string enderecoEmail, string numerosTelefones, Classe classe)
+        public async Task<IActionResult> CadastrarAluno(int matricula, string nome,  string dataNascimento, 
+        string enderecoEmail, string numerosTelefones, ClasseModel classe)
         {
+            // Implementar classe.
+            
             if(! _alunoService.ValidarEmailAsync(enderecoEmail).Result)
                 throw new AddressEmailInvalidException(enderecoEmail);
             
             var numeros = new List<string>();
             numeros.Add(numerosTelefones);
 
-            await _alunoService.CadastrarAlunoAsync(ConverterIntString(matricula), nome, dataNascimento, enderecoEmail, numeros, classe);         
+            await _alunoService.CadastrarAlunoAsync(ConverterIntString(matricula), nome, ConverterStringDateTime(dataNascimento), enderecoEmail, numeros, classe);         
 
             return Ok("Aluno cadastrado com sucesso.");
         }
@@ -98,6 +106,21 @@ namespace Disparo.Plataforma.Api.Controllers
                 result = Convert.ToString(content);
             else
                 result = "Insira uma matrícula válida, por favor!";
+
+            return result;
+        }
+
+        /// <summary>Converte uma string em datetime.</summary>
+        /// <param name="content">Conteúdo a ser convertido.</param>
+        /// <returns>Uma datetime válida ou mensagem para o usuário.</returns>
+        private DateTime ConverterStringDateTime(string content)
+        {
+            DateTime result = new DateTime();
+            
+            if(content != null)
+                result = DateTime.Parse(content);
+            else
+                Json("Insira uma data válida, por favor!");
 
             return result;
         }
