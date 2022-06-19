967054e0db3e634c1329a193f696dc58feb8a00c
Sebastião Willian Pereira Regi <swillian2009@gmail.com>
2022-06-18 23:27:22 -0300

    Correção na mensagem da exception do prédio.

--- src/Domain/Predios/Exceptions/IdentificationNumberInvalidException.cs
+++ src/Domain/Predios/Exceptions/IdentificationNumberInvalidException.cs
@@ -8,6 +8,6 @@ namespace Disparo.Plataforma.Domain.Predios.Exceptions
         /// <summary>Construtor com parâmetros para inicialização.</summary>
         /// <param name="invalidNumber">Endereço de e-mail inválido.</param>dd
         public IdentificationNumberInvalidException(int invalidNumber) :
-            base($"O número do prédio {invalidNumber} está incorreto.") {}
+            base($"O número do prédio {invalidNumber} já existe na base da dados.") {}
     }
 }
